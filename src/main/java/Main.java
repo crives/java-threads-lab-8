176a240e6af7f35548a80fc43106ce1d79618368
Caroline Rives <car.rives@gmail.com>
2022-07-27 15:40:39 -0400

    Futures lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
 import java.util.List;
 import java.util.concurrent.Future;
 
+/*
+Implement the countFinishedFutures which takes in a List of
+Future objects and counts how many of the objects completed.
+ */
 public class Main {
+    static int count = 0;
     public static int countFinishedFutures(List<Future> futures) {
-        // your code here
+        futures.forEach((future) -> {
+            if (future.isDone()) {
+                count++;
+            }
+        });
+        return count;
     }
 }
